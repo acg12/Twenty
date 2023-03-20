e191d8944937f03852d8a258bc06f7d8f601f660
Angela <angela.christabel@gmail.com>
2023-03-20 17:06:53 +0700

    Initial Commit

---
+++ Twenty Watch App/TwentyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwentyApp.swift
+//  Twenty Watch App
+//
+//  Created by Angela Christabel on 20/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Twenty_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
